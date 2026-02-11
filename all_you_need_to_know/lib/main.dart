b7da8d481f92aa8c7ab83b526e8f260adadb0cdb
Okai Tettey-Antie Samuel <o.t.a.s@Okais-MacBook-Pro.local>
2026-02-11 01:22:01 +0000

    Template-set

--- all_you_need_to_know/lib/main.dart
+++ all_you_need_to_know/lib/main.dart
@@ -20,3 +20,25 @@ class MyApp extends StatelessWidget{
     );
   }
 }
+
+class RootPage extends StatefulWidget{
+  const RootPage ({super.key});
+
+//This Syntax can substitute the syntax beneath the @override.
+  // State<name> createState() {
+  //   _nameState();
+  // }
+
+  @override
+  State<RootPage>createState() => _RootPageState();
+
+}
+
+class _RootPageState extends State<RootPage>{
+  @override
+  Widget build(BuildContext context){
+
+    return Container();
+
+  }
+}
